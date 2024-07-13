1334b5cd56222afe6c0a72d3f81766675fe53b36
Charlie Newlinds <cnewlinds@gmail.com>
2024-07-13 17:56:37 +1000

    solution code

--- server/app.py
+++ server/app.py
@@ -39,33 +39,36 @@ def games():
 
     return response
 
-@app.route('/games/<int:id>')
-def game_by_id(id):
-    game = Game.query.filter(Game.id == id).first()
-    
-    game_dict = game.to_dict()
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
 
-    response = make_response(
-        game_dict,
-        200
-    )
-
-    return response
-
-@app.route('/reviews')
-def reviews():
-
-    reviews = []
-    for review in Review.query.all():
+    if request.method == 'GET':
         review_dict = review.to_dict()
-        reviews.append(review_dict)
 
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            review_dict,
+            200
+        )
 
-    return response
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."    
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+    
 
 @app.route('/users')
 def users():
